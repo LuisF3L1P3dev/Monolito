289ff3519bdb8361fcaf6752b5bbe2fe9d722946
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-05-28 20:09:53 -0300

    feat: add rotas dos apps no projeto

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cardapio/', include('cardapio.urls')),
+    path('pedidos/', include('pedidos.urls')),
+    path('pagamento/', include('pagamento.urls')),
 ]
