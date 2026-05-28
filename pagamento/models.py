93a760c1af3364c38fe1a1403a23cc039a37d023
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-05-28 19:06:51 -0300

    feat: cricao dos models para os apps cardapio, pedido, notificao e pagamento

--- pagamento/models.py
+++ pagamento/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from pedidos.models import Pedido
 # Create your models here.
+class Transacao(models.Model):
+    STATUS_PAGAMENTO = [
+        ('PROCESSANDO', 'Processando'),
+        ('APROVADO', 'Aprovado'),
+        ('RECUSADO', 'Recusado'),]
+
+    pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE, related_name='transacao')
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=STATUS_PAGAMENTO, default='PROCESSANDO')
+    data_transacao = models.DateTimeField(auto_now_add=True)
+   
+    def __str__(self):
+       return f"Transação Pedido #{self.pedido.id} - {self.status}"
--- pedidos/models.py
+++ pedidos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from cardapio.models import ItemCardapio
 
 # Create your models here.
+class Pedido(models.Model):
+    STATUS_CHOICES = [
+        ('PENDENTE', 'Pendente'),
+        ('PAGO', 'Pago'),
+        ('ENVIADO_COZINHA', 'Enviado à Cozinha'),
+        ('CANCELADO', 'Cancelado'),]
+
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDENTE')
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"Pedido #{self.id} - {self.status}"
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, related_name='itens', on_delete=models.CASCADE)
+    item_cardapio = models.ForeignKey(ItemCardapio, on_delete=models.PROTECT)
+    quantidade = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.quantidade}x {self.item_cardapio.nome} (Pedido #{self.pedido.id})"
